50c780596ee9872488e6488aac17da453823bf3d
DESKTOP-D8DOH54\Zhe <s>
2023-05-06 23:39:02 +0800

    add Hotel,Room Controller

---
+++ Napping_PJ/Napping_PJ/Areas/Admin/Controllers/HotelsController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Napping_PJ.Models.Entity;
+
+namespace Napping_PJ.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HotelsController : Controller
+    {
+        private readonly db_a989f8_nappingContext _context;
+
+        public HotelsController(db_a989f8_nappingContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Hotels
+        public async Task<IActionResult> Index()
+        {
+              return _context.Hotels != null ? 
+                          View(await _context.Hotels.ToListAsync()) :
+                          Problem("Entity set 'db_a989f8_nappingContext.Hotels'  is null.");
+        }
+
+        // GET: Admin/Hotels/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Hotels == null)
+            {
+                return NotFound();
+            }
+
+            var hotel = await _context.Hotels
+                .FirstOrDefaultAsync(m => m.HotelId == id);
+            if (hotel == null)
+            {
+                return NotFound();
+            }
+
+            return View(hotel);
+        }
+
+        // GET: Admin/Hotels/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Hotels/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("HotelId,Name,Star,Image,ContactName,Phone,Email,City,Region,Address,AvgComment")] Hotel hotel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(hotel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(hotel);
+        }
+
+        // GET: Admin/Hotels/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Hotels == null)
+            {
+                return NotFound();
+            }
+
+            var hotel = await _context.Hotels.FindAsync(id);
+            if (hotel == null)
+            {
+                return NotFound();
+            }
+            return View(hotel);
+        }
+
+        // POST: Admin/Hotels/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("HotelId,Name,Star,Image,ContactName,Phone,Email,City,Region,Address,AvgComment")] Hotel hotel)
+        {
+            if (id != hotel.HotelId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(hotel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!HotelExists(hotel.HotelId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(hotel);
+        }
+
+        // GET: Admin/Hotels/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Hotels == null)
+            {
+                return NotFound();
+            }
+
+            var hotel = await _context.Hotels
+                .FirstOrDefaultAsync(m => m.HotelId == id);
+            if (hotel == null)
+            {
+                return NotFound();
+            }
+
+            return View(hotel);
+        }
+
+        // POST: Admin/Hotels/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Hotels == null)
+            {
+                return Problem("Entity set 'db_a989f8_nappingContext.Hotels'  is null.");
+            }
+            var hotel = await _context.Hotels.FindAsync(id);
+            if (hotel != null)
+            {
+                _context.Hotels.Remove(hotel);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool HotelExists(int id)
+        {
+          return (_context.Hotels?.Any(e => e.HotelId == id)).GetValueOrDefault();
+        }
+    }
+}
