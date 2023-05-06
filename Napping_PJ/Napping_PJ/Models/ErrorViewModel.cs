8637586e24bff009734bcea9e27c8bd90e8ec3ea
a5345534 <65459035+a5345534@users.noreply.github.com>
2023-05-06 13:33:29 +0800

    第二次刪除

--- Napping_PJ/Napping_PJ/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Napping_PJ.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
