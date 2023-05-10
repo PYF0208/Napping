f8427ea39a1575a4d20006df8bbdf11e321cb47e
JunzengWu <jim99851205@gmail.com>
2023-05-11 01:40:22 +0800

    no message

---
+++ Napping_PJ/Napping_PJ/Areas/Admin/Models/PromotionViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Napping_PJ.Models.Entity;
+
+public partial class PromotionViewModel
+{
+    
+    public int PromotionId { get; set; }
+    public int LevelId { get; set; }
+    public string Name { get; set; } = null!;
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public double Discount { get; set; }
+
+}
--- Napping_PJ/Napping_PJ/Program.cs
+++ Napping_PJ/Napping_PJ/Program.cs
@@ -1,4 +1,3 @@
-using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 using Napping_PJ.Models.Entity;
 
@@ -13,28 +12,9 @@ namespace Napping_PJ
             // Add services to the container.
             var connectionString = builder.Configuration.GetConnectionString("db_a989f8_napping_admin") ?? throw new InvalidOperationException("Connection string 'db_a989f8_napping_admin' not found.");
             builder.Services.AddDbContext<db_a989f8_nappingContext>(options =>
-                options.UseSqlServer(connectionString));            
+                options.UseSqlServer(connectionString));
 
             builder.Services.AddControllersWithViews();
-
-            // 加入身份驗證服務
-            builder.Services.AddAuthentication(o =>
-            {
-                o.DefaultScheme = "Application";
-                o.DefaultSignInScheme = "External";
-            })
-            .AddCookie("Application", options =>
-            {
-                options.LoginPath = "/Register/Register";
-                options.AccessDeniedPath = "/Path/To/Your/AccessDeniedPage";
-            })
-            .AddCookie("External")
-            .AddGoogle(options =>
-            {
-                IConfigurationSection googleAuthNSection = builder.Configuration.GetSection("Authentication:Google");
-                options.ClientId = googleAuthNSection["ClientId"];
-                options.ClientSecret = googleAuthNSection["ClientSecret"];
-            });
 
             var app = builder.Build();
 
@@ -51,29 +31,14 @@ namespace Napping_PJ
 
             app.UseRouting();
 
-            app.UseAuthentication();
             app.UseAuthorization();
 
-<<<<<<< HEAD
-
-=======
->>>>>>> f670704fae36fc0bfb64f40b217222e71eba10a4
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapAreaControllerRoute(
-                    name: "Admin",
-                    areaName: "Admin",
-                    pattern: "Admin/{controller=Home}/{action=Index}/{id?}");
-
                 endpoints.MapControllerRoute(
-<<<<<<< HEAD
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-=======
                   name: "Admin",
                   pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
                 );
->>>>>>> f670704fae36fc0bfb64f40b217222e71eba10a4
             });
             app.MapControllerRoute(
                 name: "default",
