e228c714512654802fcafff1fc06ba4c4a5c0204
ShawnChen0310 <na0953866283@gmail.com>
2023-05-11 10:54:05 +0800

    Revert "5/11還原"
    This reverts commit 6d675c6fbb29b193757ab5df28808db7f1e8ed63.

---
+++ Napping_PJ/Models/Entity/ExtraService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Napping_PJ.Models.Entity
+{
+    public partial class ExtraService
+    {
+        public ExtraService()
+        {
+            OrderDetailExtraServices = new HashSet<OrderDetailExtraService>();
+        }
+
+        public int ExtraServiceId { get; set; }
+        public int HotelId { get; set; }
+        public string Name { get; set; } = null!;
+        public double Price { get; set; }
+
+        public virtual Hotel Hotel { get; set; } = null!;
+        public virtual ICollection<OrderDetailExtraService> OrderDetailExtraServices { get; set; }
+    }
+}
---
+++ Napping_PJ/Program.cs
@@ -0,0 +1,72 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+using Napping_PJ.Models.Entity;
+
+namespace Napping_PJ
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            var connectionString = builder.Configuration.GetConnectionString("db_a989f8_napping_admin") ?? throw new InvalidOperationException("Connection string 'db_a989f8_napping_admin' not found.");
+            builder.Services.AddDbContext<db_a989f8_nappingContext>(options =>
+                options.UseSqlServer(connectionString));            
+
+            builder.Services.AddControllersWithViews();
+
+            // 加入身份驗證服務
+            builder.Services.AddAuthentication(o =>
+            {
+                o.DefaultScheme = "Application";
+                o.DefaultSignInScheme = "External";
+            })
+            .AddCookie("Application", options =>
+            {
+                options.LoginPath = "/Register/Register";
+                options.AccessDeniedPath = "/Path/To/Your/AccessDeniedPage";
+            })
+            .AddCookie("External")
+            .AddGoogle(options =>
+            {
+                IConfigurationSection googleAuthNSection = builder.Configuration.GetSection("Authentication:Google");
+                options.ClientId = googleAuthNSection["ClientId"];
+                options.ClientSecret = googleAuthNSection["ClientSecret"];
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapAreaControllerRoute(
+                    name: "Admin",
+                    areaName: "Admin",
+                    pattern: "Admin/{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+            app.Run();
+        }
+    }
+}
