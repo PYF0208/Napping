4108209603011a6dd795df3adabf5881982d4f2d
PYF <kurosaki0208@gmail.com>
2023-05-19 17:29:30 +0800

    更新資料庫

--- Napping_PJ/Napping_PJ/Models/Entity/db_a989f8_nappingContext.cs
+++ Napping_PJ/Napping_PJ/Models/Entity/db_a989f8_nappingContext.cs
@@ -180,6 +180,8 @@ namespace Napping_PJ.Models.Entity
 
                 entity.Property(e => e.ContactName).HasMaxLength(50);
 
+                entity.Property(e => e.Description).HasMaxLength(50);
+
                 entity.Property(e => e.Email).HasMaxLength(50);
 
                 entity.Property(e => e.Image).IsUnicode(false);
