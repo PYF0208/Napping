4500e4b98588a09a9a85ecabaee2d921bdc5c6d8
JunzengWu <jim99851205@gmail.com>
2023-05-22 17:34:32 +0800

    修正 Promotion日期格式問題，新增依照會員等級寄送會員促銷功能

---
+++ Napping_PJ/Napping_PJ/Areas/Admin/Controllers/中和區.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Napping_PJ.Areas.Admin.Models;
+
+namespace Napping_PJ.Areas.Admin.Controllers
+{
+	public class 中和區 : Controller
+	{
+		public IActionResult 中正路167號(SendemailViewModel promotoinviewmodel)
+		{
+			return View();
+		}
+	}
+}
