e8acaeec9610913543647c28af0b4fd1db05a4d0
ShawnChen0310 <na0953866283@gmail.com>
2023-05-23 15:51:38 +0800

    完成CRUD版型bootstrap4轉5，並完成會員資料維護CRUD

---
+++ Napping_PJ/Napping_PJ/Areas/Admin/Controllers/CustomersController.cs
@@ -0,0 +1,164 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Napping_PJ.Areas.Admin.Models;
+using Napping_PJ.Models.Entity;
+using System.Drawing.Printing;
+using System.Text.Json;
+
+namespace Napping_PJ.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CustomersController : Controller
+    {
+        private readonly db_a989f8_nappingContext _context;
+
+        public CustomersController(db_a989f8_nappingContext context)
+        {
+            _context = context;
+        }
+        public ActionResult Index()
+        {
+            int pageSize = 5;
+            int total = _context.Customers.Count();
+            int maxPage = (int)Math.Ceiling((decimal)_context.Customers.Count() / (decimal)pageSize);
+            ViewBag.maxPage = maxPage;
+            ViewBag.total = total;
+            return View();
+        }
+        public async Task<IActionResult> SkipPage(int pageIndex = 1, string filterKeyword = "")
+        {
+            filterKeyword = filterKeyword ?? "";
+            int pageSize = 5;
+            int pageNumber = pageIndex;
+            List<CustomersViewModel> customersViewModels = new List<CustomersViewModel>();
+            await _context.Customers.Where(c => c.Email.Contains(filterKeyword) || c.City.Contains(filterKeyword) || c.Country.Contains(filterKeyword) || c.Name.Contains(filterKeyword) || c.Phone.Contains(filterKeyword) || c.Region.Contains(filterKeyword)).Skip((pageNumber - 1) * pageSize).Take(pageSize).ForEachAsync(x =>
+            {
+                customersViewModels.Add(new CustomersViewModel()
+                {
+                    birthday = x.Birthday,
+                    city = x.City,
+                    country = x.Country,
+                    customerId = x.CustomerId,
+                    email = x.Email,
+                    gender = x.Gender,
+                    levelId = x.LevelId,
+                    locked = x.Locked,
+                    name = x.Name,
+                    phone = x.Phone,
+                    region = x.Region,
+                });
+            });
+             return Ok(customersViewModels);
+        }
+        public async Task<IActionResult> GetLevealList()
+        {
+            List<Level> levelList = new List<Level>();
+            await _context.Levels.ForEachAsync(l =>
+            {
+                levelList.Add(new Level()
+                {
+                    levelId = l.LevelId,
+                    name = l.Name,
+                });
+            });
+            return new JsonResult(levelList);
+        }
+        public class Level
+        {
+            public int levelId { get; set; }
+            public string name { get; set; }
+
+        }
+        public async Task<IActionResult> RemoveCuctomer([FromBody] CustomersViewModel customersViewModel)
+        {
+            Customer getCustomer = await _context.Customers.FirstOrDefaultAsync(c => c.Email == customersViewModel.email);
+            if (getCustomer == null)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                _context.Customers.Remove(getCustomer);
+                _context.SaveChanges();
+            }
+            catch (DbUpdateException ex)
+            {
+                return BadRequest(ex);
+            }
+            return Ok();
+        }
+        public async Task<IActionResult> EditCustomer([FromBody] CustomersViewModel customersViewModel)
+        {
+
+            Customer getCustomer = await _context.Customers.FirstOrDefaultAsync(c => c.Email == customersViewModel.email);
+            if (getCustomer == null)
+            {
+                return BadRequest();
+            }
+            getCustomer.Name = customersViewModel.name;
+            getCustomer.Birthday = customersViewModel.birthday;
+            getCustomer.Phone = customersViewModel.phone;
+            getCustomer.Gender = customersViewModel.gender;
+            getCustomer.City = customersViewModel.city;
+            getCustomer.Region = customersViewModel.region;
+            getCustomer.Country = customersViewModel.country;
+            getCustomer.Email = customersViewModel.email;
+            getCustomer.LevelId = customersViewModel.levelId;
+            getCustomer.Locked = customersViewModel.locked;
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+
+            return Ok();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateCustomer([FromBody] CustomersViewModel customersViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            Customer getCustomer = await _context.Customers.FirstOrDefaultAsync(c => c.Email == customersViewModel.email);
+            if (getCustomer != null)
+            {
+                return BadRequest();
+            }
+            Customer newCustomer = new Customer();
+            newCustomer.Name = customersViewModel.name;
+            newCustomer.Birthday = customersViewModel.birthday;
+            newCustomer.Phone = customersViewModel.phone;
+            newCustomer.Gender = customersViewModel.gender;
+            newCustomer.City = customersViewModel.city;
+            newCustomer.Region = customersViewModel.region;
+            newCustomer.Country = customersViewModel.country;
+            newCustomer.Email = customersViewModel.email;
+            newCustomer.LevelId = customersViewModel.levelId;
+            newCustomer.Locked = customersViewModel.locked;
+            try
+            {
+                _context.Customers.Add(newCustomer);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+
+            return Ok();
+        }
+        [HttpPost]
+        public async Task<IActionResult> ValidFileds([FromBody] CustomersViewModel customersViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            return Ok(ModelState);
+        }
+    }
+}
---
+++ Napping_PJ/Napping_PJ/Areas/Admin/Models/CustomersViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Napping_PJ.Areas.Admin.Models
+{
+    public class CustomersViewModel
+    {
+        public int customerId { get; set; }
+        [Required(ErrorMessage = "請輸入姓名")]
+        public string? name { get; set; }
+        [Required(ErrorMessage = "birthday為必填欄位")]
+        public DateTime? birthday { get; set; }
+        [Required(ErrorMessage = "請輸入電話")]
+        public string? phone { get; set; }
+        [Required(ErrorMessage = "請選擇性別")]
+        public bool? gender { get; set; }
+        [Required(ErrorMessage = "請選擇城市")]
+        public string? city { get; set; }
+        [Required(ErrorMessage = "請選擇地區")]
+        public string? region { get; set; }
+        [Required(ErrorMessage = "請輸入地址")]
+        public string? country { get; set; }
+        [Required(ErrorMessage = "email為必填欄位")]
+        [EmailAddress(ErrorMessage = "須符合信箱格式")]
+        public string? email { get; set; }
+        [Required(ErrorMessage = "請賦予會員等級")]
+        public int? levelId { get; set; }
+        [Required(ErrorMessage = "請設定鎖定狀態")]
+        public bool? locked { get; set; }
+    }
+}
