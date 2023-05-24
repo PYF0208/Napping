8496f6d982ca4ba2b4c9694fd3a98994830b2b5d
DESKTOP-D8DOH54\Zhe <s>
2023-05-25 02:51:02 +0800

    add order CRUD

--- Napping_PJ/Napping_PJ/Areas/Admin/Controllers/OrdersController.cs
+++ Napping_PJ/Napping_PJ/Areas/Admin/Controllers/OrdersController.cs
@@ -12,189 +12,225 @@ using Napping_PJ.Models.Entity;
 
 namespace Napping_PJ.Areas.Admin.Controllers
 {
-	[Area("Admin")]
-	public class OrdersController : Controller
-	{
-		private readonly db_a989f8_nappingContext _context;
+    [Area("Admin")]
+    public class OrdersController : Controller
+    {
+        private readonly db_a989f8_nappingContext _context;
 
-		public OrdersController(db_a989f8_nappingContext context)
-		{
-			_context = context;
-		}
+        public OrdersController(db_a989f8_nappingContext context)
+        {
+            _context = context;
+        }
 
-		// GET: Admin/Orders
-		public async Task<IActionResult> Index()
-		{
-			return View();
-		}
+        // GET: Admin/Orders
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
 
-		// GET: Admin/Orders/Details/5
-		[HttpGet]
-		public async Task<IEnumerable<OrdersViewModel>> GetOrder()
-		{
-			var ord = _context.Orders.Include(o=>o.OrderDetails).Include(x=>x.Customer).Include(x=>x.Payment).Select(o => new OrdersViewModel
-			{
-				
-				CustomerId = o.CustomerId,
-				OrderId = o.OrderId,
-				PaymentId = o.PaymentId,
-				Date = o.Date,
+        // GET: Admin/Orders/Details/5
+        [HttpGet]
+        public async Task<IEnumerable<OrdersViewModel>> GetOrder()
+        {
+            var ord = _context.Orders.Include(o => o.OrderDetails).Include(x => x.Customer).Include(x => x.Payment).Select(o => new OrdersViewModel
+            {
+
+                CustomerId = o.CustomerId,
+                OrderId = o.OrderId,
+                PaymentId = o.PaymentId,
+                Date = o.Date,
                 CustomerName = o.Customer.Name,
-			
-			});
-			
-			return ord;
-		}
-		[HttpGet]
-		//public async Task<IEnumerable<PaymentViewModel>> GetPayment()
-		//{
-		//	_context
-		//}
-		[HttpGet]
-		public async Task<IEnumerable<MemberViewModel>> GetMember()
-		{
-			var Mem = _context.Customers.Select(c => new MemberViewModel
-			{
-				Birthday = c.Birthday,
-				City = c.City,
-				Country = c.Country,
-				Email = c.Email,
-				Gender = c.Gender,
-				LevelId = c.LevelId,
-				Phone = c.Phone,
-				Region = c.Region,
-				Name = c.Name,
-			});
-			return Mem;
-		}
-		public async Task<IActionResult> Details(int? id)
-		{
-			if (id == null || _context.Orders == null)
-			{
-				return NotFound();
-			}
 
-			var order = await _context.Orders
-				
-				.Include(o => o.Customer)
-				.Include(o => o.Payment)
-				.FirstOrDefaultAsync(m => m.OrderId == id);
-			if (order == null)
-			{
-				return NotFound();
-			}
+            });
 
-			return View(order);
-		}
+            return ord;
+        }
+        [HttpGet]
+        public async Task<IEnumerable<PaymentViewModel>> GetPayment()
+        {
+            var Pa = _context.Payments.Select(o => new PaymentViewModel
+            {
+                Date = o.Date,
+                OrderId = o.OrderId,
+                PaymentId = o.PaymentId,
+                Status = o.Status,
+                Type = o.Type,
+            });
+            return Pa;
+        }
+        [HttpGet]
+        public async Task<IEnumerable<MemberViewModel>> GetMember()
+        {
+            var Mem = _context.Customers.Select(c => new MemberViewModel
+            {
+                Birthday = c.Birthday,
+                City = c.City,
+                Country = c.Country,
+                Email = c.Email,
+                Gender = c.Gender,
+                LevelId = c.LevelId,
+                Phone = c.Phone,
+                Region = c.Region,
+                Name = c.Name,
+                CustomerId = c.CustomerId,
+            });
+            return Mem;
+        }
+        [HttpGet]
+        public async Task<IEnumerable<OrderDetailsViewModel>> GetOrderDetails()
+        {
+            var od = _context.OrderDetails.Select(od => new OrderDetailsViewModel
+            {
+                OrderDetailId = od.OrderDetailId,
+                OrderId = od.OrderDetailId,
+                RoomId = od.RoomId,
+                ProfitId = od.ProfitId,
+                CheckIn = od.CheckIn,
+                CheckOut = od.CheckOut,
+                NumberOfGuests = od.NumberOfGuests,
+                TravelType = od.TravelType,
+                Note = od.Note,
+            });
+            return od;
+        }
 
-		// GET: Admin/Orders/Create
-	
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Orders == null)
+            {
+                return NotFound();
+            }
 
-		// POST: Admin/Orders/Create
-		// To protect from overposting attacks, enable the specific properties you want to bind to.
-		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-		[HttpPost]
+            var order = await _context.Orders
 
-		public async Task<string> Create([FromBody] OrdersViewModel order)
-		{
-			var o = await _context.Orders.FirstOrDefaultAsync(o => o.OrderId == order.OrderId);
-			if (o != null)
-			{
-				var Neworder = new Order
-				{
-					OrderId = order.OrderId,
-					CustomerId = order.CustomerId,
-					Date = order.Date,
-					PaymentId = order.PaymentId,
+                .Include(o => o.Customer)
+                .Include(o => o.Payment)
+                .FirstOrDefaultAsync(m => m.OrderId == id);
+            if (order == null)
+            {
+                return NotFound();
+            }
 
-				};
-				_context.Add(Neworder);
-				await _context.SaveChangesAsync();
-				return "創建成功";
-			}
-			return "創建失敗";
-		}
+            return View(order);
+        }
 
-		// GET: Admin/Orders/Edit/5
-		
-
-		// POST: Admin/Orders/Edit/5
-		// To protect from overposting attacks, enable the specific properties you want to bind to.
-		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-		[HttpPost]
-
-		public async Task<string> Edit(int id, [FromBody] OrdersViewModel order)
-		{
-			var ord = await _context.Orders.FindAsync(id);
-			if (id != order.OrderId && ord.OrderId!=order.OrderId)
-			{
-				return "修改失敗";
-			}
-			try
-			{
-				_context.Update(ord);
-				await _context.SaveChangesAsync();
-			}
-			catch (DbUpdateConcurrencyException)
-			{
-				if (!OrderExists(ord.OrderId))
-				{
-					return "修改失敗";
-				}
-				else
-				{
-					throw;
-				}
-			}
-			return "修改成功";
-		}
+        // GET: Admin/Orders/Create
 
 
+        // POST: Admin/Orders/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
 
-		// GET: Admin/Orders/Delete/5
-		public async Task<IActionResult> Delete(int? id)
-		{
-			if (id == null || _context.Orders == null)
-			{
-				return NotFound();
-			}
+        public async Task<string> Create([FromBody] OrdersViewModel order)
+        {
+            try
+            {
+                _context.Orders.Add(new Order()
+                {
+                    CustomerId = order.CustomerId,
+                    PaymentId = order.PaymentId,
+                    Date = order.Date,
 
-			var order = await _context.Orders
-				.Include(o => o.Customer)
-				.Include(o => o.Payment)
-				.FirstOrDefaultAsync(m => m.OrderId == id);
-			if (order == null)
-			{
-				return NotFound();
-			}
+                });
+                _context.SaveChanges();
+                return "創建成功";
 
-			return View(order);
-		}
+            }
+            catch (Exception)
+            {
 
-		// POST: Admin/Orders/Delete/5
-		[HttpDelete]
+                return "創建失敗";
+            }
 
-		public async Task<string> DeleteConfirmed(int id)
-		{
-			var or = await _context.Orders.FindAsync(id);
-			if (or == null)
-			{
-				return "刪除失敗";
-			}
 
-			if (or != null)
-			{
-				_context.Orders.Remove(or);
-			}
+        }
 
-			await _context.SaveChangesAsync();
-			return "刪除成功";
-		}
 
-		private bool OrderExists(int id)
-		{
-			return (_context.Orders?.Any(e => e.OrderId == id)).GetValueOrDefault();
-		}
-	}
+        // GET: Admin/Orders/Edit/5
+
+
+        // POST: Admin/Orders/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+
+        public async Task<string> Edit(int id, [FromBody] OrdersViewModel order)
+        {
+            var ord = await _context.Orders.FindAsync(id);
+            if (id != order.OrderId && ord.OrderId != order.OrderId)
+            {
+                return "修改失敗";
+            }
+            ord.OrderId = order.OrderId;
+            ord.CustomerId = order.CustomerId;
+            ord.PaymentId = order.PaymentId;
+            ord.Date = order.Date;
+            try
+            {
+                _context.Update(ord);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OrderExists(ord.OrderId))
+                {
+                    return "修改失敗";
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return "修改成功";
+        }
+
+
+
+        // GET: Admin/Orders/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Orders == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Orders
+                .Include(o => o.Customer)
+                .Include(o => o.Payment)
+                .FirstOrDefaultAsync(m => m.OrderId == id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return View(order);
+        }
+
+        // POST: Admin/Orders/Delete/5
+        [HttpDelete]
+
+        public async Task<string> DeleteConfirmed(int id)
+        {
+            var or = await _context.Orders.FindAsync(id);
+            if (or == null)
+            {
+                return "刪除失敗";
+            }
+
+            if (or != null)
+            {
+                _context.Orders.Remove(or);
+            }
+
+            await _context.SaveChangesAsync();
+            return "刪除成功";
+        }
+
+        private bool OrderExists(int id)
+        {
+            return (_context.Orders?.Any(e => e.OrderId == id)).GetValueOrDefault();
+        }
+    }
 }
 
--- Napping_PJ/Napping_PJ/Models/Entity/db_a989f8_nappingContext.cs
+++ Napping_PJ/Napping_PJ/Models/Entity/db_a989f8_nappingContext.cs
@@ -170,7 +170,6 @@ namespace Napping_PJ.Models.Entity
 
                 entity.Property(e => e.ContactName).HasMaxLength(50);
 
-
                 entity.Property(e => e.Email)
                     .HasMaxLength(50)
                     .IsUnicode(false);
@@ -180,13 +179,6 @@ namespace Napping_PJ.Models.Entity
                 entity.Property(e => e.Phone)
                     .HasMaxLength(50)
                     .IsUnicode(false);
-
-                entity.Property(e => e.Email).HasMaxLength(50);
-
-                entity.Property(e => e.Name).HasMaxLength(50);
-
-                entity.Property(e => e.Phone).HasMaxLength(50);
-
 
                 entity.Property(e => e.Region).HasMaxLength(50);
 
