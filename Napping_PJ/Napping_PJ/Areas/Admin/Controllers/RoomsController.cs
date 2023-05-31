887853d0a41efde5f27fbee80cc77db748b624c4
JunzengWu <jim99851205@gmail.com>
2023-05-31 20:04:03 +0800

    房型管理新增房型特徵

--- Napping_PJ/Napping_PJ/Areas/Admin/Controllers/RoomsController.cs
+++ Napping_PJ/Napping_PJ/Areas/Admin/Controllers/RoomsController.cs
@@ -30,7 +30,7 @@ namespace Napping_PJ.Areas.Admin.Controllers
 			//var db_a989f8_nappingContext = _context.Rooms.Include(r => r.Hotel);
 			//var db_a989f8_nappingContext2 = db_a989f8_nappingContext.Select(s=>s.Hotel);
 			//return View(await db_a989f8_nappingContext.ToListAsync());
-			var Rooms = await _context.Rooms.Include(p => p.Hotel)
+			var Rooms = await _context.Rooms.Include(p => p.Hotel).Include(p => p.Features)
 			.Select(Rooms => new RoomsViewModel
 			{
 				RoomId = Rooms.RoomId,
@@ -39,16 +39,98 @@ namespace Napping_PJ.Areas.Admin.Controllers
 				Price = Rooms.Price,
 				MaxGuests = Rooms.MaxGuests,
 
+
 			}).ToListAsync();
 			return Rooms;
 		}
+		[HttpPost]
+		public async Task<string> CreateFeature([FromBody] FeatureViewModel feature)
+		{
+			Feature NewFeature = new Feature
+			{
+				FeatureId = feature.FeatureId,
+				Name = feature.Name,
+				Image = feature.Image,
+			};
+			_context.Features.Add(NewFeature);
+			await _context.SaveChangesAsync();
+			return "新增成功";
+		}
+		[HttpGet]
+		public async Task<IEnumerable<FeatureViewModel>> GetFeature()
+		{
+			var Feature = await _context.Features
+				.Select(Feature => new FeatureViewModel
+				{
+					FeatureId = Feature.FeatureId,
+					Name = Feature.Name,
+					Image = Feature.Image
 
+				})
+				.ToListAsync();
+			return Feature;
+		}
+
+		[HttpPut]
+		public async Task<string> Feature(int id, [FromBody] FeatureViewModel feature)
+		{
+			if (id != feature.FeatureId)
+			{
+				return "修改失敗！";
+			}
+
+			Feature ft = await _context.Features.FindAsync(feature.FeatureId);
+			ft.FeatureId = feature.FeatureId;
+			ft.Name = feature.Name;
+			ft.Image = feature.Image;
+			_context.Entry(ft).State = EntityState.Modified;
+
+			try
+			{
+				_context.Update(ft);
+				await _context.SaveChangesAsync();
+			}
+			catch (DbUpdateConcurrencyException)
+			{
+				if (!FeatureExists(id))
+				{
+					return "修改失敗！";
+				}
+				else
+				{
+					throw;
+				}
+			}
+
+			return "修改成功！";
+		}
+		[HttpDelete]
+		public async Task<string> DeleteFeature(int id)
+		{
+			var feature = await _context.Features.FindAsync(id);
+			if (feature == null)
+			{
+				return "刪除特徵成功!";
+			}
+
+			_context.Features.Remove(feature);
+			try
+			{
+				await _context.SaveChangesAsync();
+			}
+			catch (DbUpdateException ex)
+			{
+				return "刪除特徵記錄失敗!";
+			}
+
+			return "刪除特徵記錄成功!";
+		}
 		[HttpPost]
 
 		public async Task<IEnumerable<RoomsViewModel>> FilterRooms(RoomsViewModel rmViewMod)
 		{
 
-			var Rooms = await _context.Rooms.Include(p => p.Hotel)
+			var Rooms = await _context.Rooms.Include(p => p.Hotel).Include(x => x.Features)
 			.Select(Rooms => new RoomsViewModel
 			{
 				RoomId = Rooms.RoomId,
@@ -56,6 +138,12 @@ namespace Napping_PJ.Areas.Admin.Controllers
 				Type = Rooms.Type,
 				Price = Rooms.Price,
 				MaxGuests = Rooms.MaxGuests,
+				Feature = Rooms.Features.Select(x => new FeatureInRoomViewModel
+				{
+					Id = x.FeatureId,
+					Name = x.Name,
+					Image = x.Image
+				}).ToList(),
 
 			}).ToListAsync();
 			return Rooms;
@@ -93,12 +181,12 @@ namespace Napping_PJ.Areas.Admin.Controllers
 		[HttpPost]
 		public async Task<string> CreateRooms([FromBody] RoomsViewModel room)
 		{
-			
+
 			Room NewRoom = new Room
 			{
 				RoomId = room.RoomId,
 				HotelId = room.HotelId,
-				Type = room.Type, 
+				Type = room.Type,
 				Price = room.Price,
 				MaxGuests = room.MaxGuests,
 			};
@@ -214,6 +302,10 @@ namespace Napping_PJ.Areas.Admin.Controllers
 		private bool RoomExists(int id)
 		{
 			return (_context.Rooms?.Any(e => e.RoomId == id)).GetValueOrDefault();
+		}
+		private bool FeatureExists(int id)
+		{
+			return (_context.Features?.Any(e => e.FeatureId == id)).GetValueOrDefault();
 		}
 	}
 }
