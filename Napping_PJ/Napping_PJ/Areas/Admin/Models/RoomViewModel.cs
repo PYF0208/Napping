d2a80a81c80681bbd39c914052e447b53797ee5c
eric91712 <eric91712@gmail.com>
2023-06-01 02:16:18 +0800

    新增地圖馾出視窗以及根據地圖顯示飯店資訊

--- Napping_PJ/Napping_PJ/Areas/Admin/Models/RoomViewModel.cs
+++ Napping_PJ/Napping_PJ/Areas/Admin/Models/RoomViewModel.cs
@@ -4,8 +4,16 @@
 	{
 		public int RoomId { get; set; }
 		public int HotelId { get; set; }
-		public string? Type { get; set; } 
+		public string? Type { get; set; }
 		public double Price { get; set; }
 		public int MaxGuests { get; set; }
+
+		public List<FeatureInRoomViewModel>Feature{get; set;}
+	}
+	public class FeatureInRoomViewModel
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Image { get; set; }
 	}
 }
