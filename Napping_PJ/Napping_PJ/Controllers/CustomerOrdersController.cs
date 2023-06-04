d19c23ab109236c2bfbff43d5dcb764102eb22a7
PYF <kurosaki0208@gmail.com>
2023-06-04 09:38:49 +0800

    前台會員訂單管理

---
+++ Napping_PJ/Napping_PJ/Controllers/CustomerOrdersController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Napping_PJ.Areas.Admin.Models;
+using Napping_PJ.Models;
+using Napping_PJ.Models.Entity;
+using System.Security.Claims;
+
+namespace Napping_PJ.Controllers
+{
+    public class CustomerOrdersController : Controller
+    {
+        private readonly db_a989f8_nappingContext _context;
+
+        public CustomerOrdersController(db_a989f8_nappingContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IEnumerable<CustomerOrdersViewModel> GetCustomerOrders()
+        {
+            var customer = _context.Customers.AsNoTracking().FirstOrDefault(x => x.Email == User.FindFirst(ClaimTypes.Email).Value);
+            var customerOrders = _context.OrderDetails.Include(x => x.Order).Include(x => x.Room).ThenInclude(x => x.Hotel).Where(x=>x.Order.CustomerId==customer.CustomerId).Select(co => new CustomerOrdersViewModel
+            {   //OrderDetails表
+                OrderId=co.OrderId,
+                OrderDetailId=co.OrderDetailId,
+                RoomId=co.RoomId,
+                CheckIn=co.CheckIn,
+                CheckOut=co.CheckOut,
+                NumberOfGuests=co.NumberOfGuests,
+                TotalPrice=co.RoomTotalPrice+co.EspriceTotal-co.DiscountTotalPrice,
+                Note=co.Note,
+                
+                //Orders表
+                NameOfBooking=co.Order.NameOfBooking,
+                OrderDate=co.Order.Date,
+
+                //Rooms表
+                RoomType=co.Room.Type,
+
+                //Hotels表
+                HotelName=co.Room.Hotel.Name,
+                HotelImage=co.Room.Hotel.Image,
+                City=co.Room.Hotel.City,
+                Region=co.Room.Hotel.Region,
+                AvgComment=co.Room.Hotel.AvgComment,
+                HotelPhone=co.Room.Hotel.Phone,
+
+                //Payments表
+                PaymentType = co.Order.Payments.OrderBy(x=>x.PaymentId).Last().Type
+            });
+
+            return customerOrders;
+        }
+    }
+
+}
---
+++ Napping_PJ/Napping_PJ/Models/CustomerOrdersViewModel.cs
@@ -0,0 +1,33 @@
+﻿namespace Napping_PJ.Models
+{
+    public class CustomerOrdersViewModel
+    {
+        public string NameOfBooking { get; set; } = null!;
+        public DateTime OrderDate { get; set; }
+        //Orders表
+
+        public int OrderId { get; set; }
+        public int OrderDetailId { get; set; }
+        public int RoomId { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+        public int NumberOfGuests { get; set; }
+        public double? TotalPrice { get; set; }
+        public string? Note { get; set; }
+        //OrderDetails表
+
+        public string RoomType { get; set; } = null!;
+        //Rooms表
+
+        public string HotelName { get; set; } = null!;
+        public string HotelImage { get; set; } = null!;
+        public string City { get; set; } = null!;
+        public string Region { get; set; } = null!;
+        public double? AvgComment { get; set; }
+        public string HotelPhone { get; set; } = null!;
+        //Hotels表
+
+        public string PaymentType { get; set; } = null!;
+        //Payments表
+    }
+}
